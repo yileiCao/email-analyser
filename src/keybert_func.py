c561d4abc67639846b15481452e6740662fda61c
YILEI Cao <yxc018@cs.bham.ac.uk>
2023-12-06 01:05:36 +0900

    Add generate keyword function in view_mail page

--- src/keybert_func.py
+++ src/keybert_func.py
@@ -1,9 +1,10 @@
 from keybert import KeyBERT
 
 
-def extract_keyword(text):
+def extract_keyword(text, kw_len_fr=1, kw_len_to=3, diversity=0.5, kw_num=5):
     kw_model = KeyBERT()
-    keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 3), use_mmr=True, diversity=0.5)
+    keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(kw_len_fr, kw_len_to),
+                                         use_mmr=True, diversity=diversity, top_n=kw_num)
     return keywords
 
 
